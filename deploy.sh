2ad09b925835432df54738016cc1b48c64c6076d
terminalgames <terminalgames@gmail.com>
2016-03-06 11:16:28 +0200

    update README.md

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 ./prod.sh
 jekyll build
 rsync -av ~/blog/terminalgames/source/_site/ ~/blog/terminalgames/live
-
+cd ~/blog/terminalgames/live
+#gitcom "update"
+#git push -u origin master
+#cd ~/blog/terminalgames/source
